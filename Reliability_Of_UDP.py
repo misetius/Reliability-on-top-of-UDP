8c6c735b0af91c29b8108604e3e21de9262d7443
Mihail <miskatapio@gmail.com>
2024-11-06 10:07:41 +0200

    Pohja Virtuaalisoketille

---
+++ Reliability_Of_UDP.py
@@ -0,0 +1,25 @@
+import socket
+
+class Virtuaalisoketti:
+    def __init__(self, portti, ip):
+        self.soketti = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        self.soketti.bind((ip, portti))
+
+    def receivedata(self):
+        while True:
+            data, addr = self.soketti.recvfrom(1024)
+            return data
+
+
+
+
+portti = 6666
+ip = "127.0.0.1"
+
+omasoketti = Virtuaalisoketti(portti, ip)
+
+kuunnellaan = True
+while kuunnellaan:
+    print(omasoketti.receivedata())
+    
+
