044b355a3a74e4d28cfe2b77078c01b7f5340a4f
Miska Malkamaki <mitamalk.jyu.fi>
2024-11-28 09:58:24 +0200

    lisätty paketin tiputtaminen

--- Reliability_Of_UDP.py
+++ Reliability_Of_UDP.py
@@ -3,14 +3,23 @@ import random
 
 
 class Virtuaalisoketti:
-    def __init__(self, portti, ip, todennakoisyys = 0.5):
+    def __init__(self, portti, ip):
         self.soketti = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         self.soketti.bind((ip, portti))
 
     def receivedata(self):
         while True:
             data, addr = self.soketti.recvfrom(1024)
-            return data
+            r1 = random.randint(0, 100)
+
+            if r1 < 50:
+                data = data[0:-1]
+                dekoodattu_data = data.decode('ascii')
+                print(dekoodattu_data)
+                
+            else:
+                print("Dropped packet")
+                continue
 
 
 
@@ -20,8 +29,6 @@ ip = "127.0.0.1"
 
 omasoketti = Virtuaalisoketti(portti, ip)
 
-kuunnellaan = True
-while kuunnellaan:
-    print(omasoketti.receivedata())
+omasoketti.receivedata()
     
 
