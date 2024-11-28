ddcb9aac8b414585ea1e0b0a29eb8eab86ade49d
Miska Malkamaki <mitamalk.jyu.fi>
2024-11-28 11:30:10 +0200

    Binäärivhirheen lisäys ja satunnainen paketin hidas käsittely

--- Reliability_Of_UDP.py
+++ Reliability_Of_UDP.py
@@ -1,6 +1,6 @@
 import socket
 import random
-
+import time
 
 class Virtuaalisoketti:
     def __init__(self, portti, ip):
@@ -11,16 +11,55 @@ class Virtuaalisoketti:
         while True:
             data, addr = self.soketti.recvfrom(1024)
             r1 = random.randint(0, 100)
+            r2 = random.randint(0, 100)
+            r3 = random.randint(0, 100)
 
-            if r1 < 50:
+            if r1 < 50 and r2<50:
                 data = data[0:-1]
-                dekoodattu_data = data.decode('ascii')
+                dekoodattu_data = data.decode('utf-8')
+                if r3 < 50:
+                    dekoodattu_data = bittivirheenlisaaminen(dekoodattu_data)
                 print(dekoodattu_data)
-                
+
+            elif r1<50 and r2 > 50:
+                data = data[0:-1]
+                dekoodattu_data = data.decode('utf-8')
+                if r3 < 50:
+                    dekoodattu_data = bittivirheenlisaaminen(dekoodattu_data)
+                time.sleep(1)
+                print(dekoodattu_data)
+
+#011011010110111101101001 = moi
             else:
                 print("Dropped packet")
-                continue
+                
 
+def bittivirheenlisaaminen(viesti):
+    binaarilista = []
+
+    for merkki in viesti:
+        binaarilista.append(bin(ord(merkki))[2:].zfill(8))
+
+    
+
+    r4 = random.randint(0, len(binaarilista)-1)
+    muutettavabitti = binaarilista[r4]
+    if muutettavabitti == '1':
+        binaarilista[r4] = '0'
+    else: 
+        binaarilista[r4] = '1'
+    
+    binaariluku = ''.join(binaarilista)
+
+    binaariviestiksi = ""
+
+    for i in range(0, len(binaariluku), 8):
+        binc = binaariluku[i:i + 8]
+        num = int(binc, 2)
+        binaariviestiksi += chr(num)
+    return binaariviestiksi
+
+    
 
 
 
