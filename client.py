0c4d01aef60f926fa30878398dd9e66e39b3a513
Miska Malkamaki <mitamalk.jyu.fi>
2024-11-29 20:02:41 +0200

    r

--- client.py
+++ client.py
@@ -9,12 +9,12 @@ udpCliSock = socket(AF_INET,SOCK_DGRAM)
 
 while True:
     viesti = input("> ")
-    viestidekoodattuna = b"{viesti}"
+    
     udpCliSock.sendto(viesti.encode(),ADDR) 
     
     
-    #recv_data,ADDR = udpCliSock.recvfrom(BUFSIZE)
-    #if recv_data is not None:
-     #  print "[%s]: receiving data from server %s:%s  :%s" %(ctime(),ADDR[0],ADDR[1],recv_data)
+    recv_data,ADDR = udpCliSock.recvfrom(1024)
     
-udpCliSock.close()
+        
+    
+
